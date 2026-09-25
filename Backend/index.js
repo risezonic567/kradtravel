5f329c684827503b28e02f49e2a87dea94c034f1
Sunny9467 <kumarsunny919200@gmail.com>
2026-09-25 18:14:17 +0530

    remove double slace

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cors({
     origin:[
         "http://localhost:5173",
-        "https://www.kradtravel.com/",
+        "https://www.kradtravel.com",
         "https://kradtravel.com"
     ],
     methods:["GET","POST","PUT","DELETE"],
