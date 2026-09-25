f893d36869c2e499c50cd23cc8292001d65ba833
Sunny9467 <kumarsunny919200@gmail.com>
2026-09-25 18:02:50 +0530

    added and checking the backend

--- Frontend/src/components/FlightDetailsPage.jsx
+++ Frontend/src/components/FlightDetailsPage.jsx
@@ -6,7 +6,7 @@ export default function FlightDetailsPage() {
     <div className= 'relative bg-gray-300/15 mt-20 min-h-screen mx-auto max-w-7xl'>
         <div className='max-w-xl p-5 m-5 py-5  bg-black/50 h-[250px]'>
             <h2 className='text-white p-5 m-4 text-center text-2xl'>
-                Flight Details
+                Flight Detail
             </h2>
             <div className="text-white">
                 <p>
